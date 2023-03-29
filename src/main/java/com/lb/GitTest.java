004f113bd9f6eb4ad711fda43e3a9b03ddb9789e
LB <1355482572@qq.com>
2023-03-29 17:13:41 +0800

    third commit

--- src/main/java/com/lb/GitTest.java
+++ src/main/java/com/lb/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
         System.out.println("lb is a hero!");
+        System.out.println("666666");
     }
 }
