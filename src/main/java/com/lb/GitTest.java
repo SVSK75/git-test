f3c71393a8d1574a711dd0e5c4cad188e2c11f93
LB <1355482572@qq.com>
2023-03-29 17:17:43 +0800

    hot-fix commit

--- src/main/java/com/lb/GitTest.java
+++ src/main/java/com/lb/GitTest.java
@@ -4,6 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
         System.out.println("lb is a hero!");
-        System.out.println("666666");
+        System.out.println("hot-fix");
     }
 }
