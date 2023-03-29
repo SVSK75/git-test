3adfe97f165734f8bdad64b32643a3e606baf8d5
LB <1355482572@qq.com>
2023-03-29 17:05:03 +0800

    second commit

--- src/main/java/com/lb/GitTest.java
+++ src/main/java/com/lb/GitTest.java
@@ -3,5 +3,6 @@ package com.lb;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("lb is a hero!");
     }
 }
